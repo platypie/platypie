9108bf1c8ddf0a4cbe6bb898da5bc0e7d6698906
ansuz <aaronmacsweenmusic@gmail.com>
2014-04-18 20:21:05 -0400

    A simple display of gratitude

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-	println!("platypie");
+  let message = ["Thanks for installing Platypus!","We're trying to make the world a better place.","We're very happy you've decided to get involved."];
+
+  for sptr in message.iter() {
+    println!("{}",*sptr);
+  }
 }
