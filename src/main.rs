34c2af52c52381ad1bc6b6db8267c86be02de2d0
ansuz <aaronmacsweenmusic@gmail.com>
2014-04-18 20:31:50 -0400

    you'd think I'd know my own project's name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
   let message = [
-    "Thanks for installing Platypus!",
+    "Thanks for installing Platypie!",
     "We're trying to make the world a better place.",
     "We're very happy you've decided to get involved.",
     "Platypie is a work in progress.",
